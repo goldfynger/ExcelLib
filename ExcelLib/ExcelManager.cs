0bfd580dc317361caf7b0a78c008a7027fe2b2b6
galfingerea <galfingerea@energomera.ru>
2015-06-15 11:33:32 +0300

    [!]ExcelLib [+]Project created

---
+++ ExcelLib/ExcelManager.cs
@@ -0,0 +1,8 @@
+﻿namespace ExcelLib
+{
+    /// <summary></summary>
+    public sealed class ExcelManager
+    {
+
+    }
+}
---
+++ _Run_ExcelLib/Program.cs
@@ -0,0 +1,15 @@
+﻿using ExcelLib;
+
+namespace _Run_ExcelLib
+{
+    /// <summary></summary>
+    class Program
+    {
+        /// <summary></summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
